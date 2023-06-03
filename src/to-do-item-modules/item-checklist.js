1231e964a6e468aeab2524c890b634b1a4f12d29
njcushing <129661299+njcushing@users.noreply.github.com>
2023-06-03 14:09:39 +0100

    Create checklistItem module

--- src/to-do-item-modules/item-checklist.js
+++ src/to-do-item-modules/item-checklist.js
@@ -1,4 +1,31 @@
-const checklist = () => {
-    return {};
+const checklistItem = (n, s) => {
+    let name = "Checklist Item";
+    if (typeof n === "string") name = n;
+    let state = false;
+    if (typeof s === "boolean") state = s;
+
+    const setName = (x) => {
+        if (typeof x !== "string") return;
+        if (x.length > 75) x = x.slice(0, 75);
+        name = x;
+    };
+    const getName = () => {
+        return name;
+    };
+
+    const setState = (x) => {
+        if (typeof x !== "boolean") return;
+        state = x;
+    };
+    const getState = () => {
+        return state;
+    };
+
+    return {
+        setName,
+        getName,
+        setState,
+        getState,
+    };
 };
-export default checklist;
+export default checklistItem;
